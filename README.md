dd77bc36df1b4517f847b69ffa65821fbdb5defa
Gokul Varadan <gokulvaradan2202@gmail.com>
2025-05-22 10:14:30 +0530

    basic drawing integrated

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
-# gamuts
+## gamuts
 
-Requirements:
+### Requirements:
 
-Decisions:
-React + Vite
-Canvas / DOM:
-Canvas is faster
-Complex to implement everything from scratch
-DOM will be less efficient and cluttered
+- Hand Drawing Mode
+- Shapes & Put text inside shape
+- Infinite Zoom in-out
+- Image support
+  - Crop, Rounded, borders
+  - draw on top of image
+  - Invert
+  - Rotate
+- Text
+
+### Decisions:
+
+- React + Vite
+- Canvas / DOM:
+  - Canvas is faster
+  - Complex to implement everything from scratch
+  - DOM will be less efficient and cluttered
+  - Let's go with Canvas
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,107 @@
 import gamutsLogo from "/gamuts_logo.svg";
 import "./App.css";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const mouseDown = useRef<React.MouseEvent | React.TouchEvent>(null);
+  const contextRef = useRef<CanvasRenderingContext2D>(null);
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 200;
+    const ctx = canvas.getContext("2d");
+    contextRef.current = ctx;
+  }, [canvasRef]);
+
+  function handleMouseDown(event: React.MouseEvent | React.TouchEvent) {
+    if (!canvasRef.current || !contextRef.current) {
+      return;
+    }
+    const rect = canvasRef.current.getBoundingClientRect();
+    let clientX = 0,
+      clientY = 0;
+    if ("touches" in event) {
+      clientX = event.touches[0].clientX;
+      clientY = event.touches[0].clientY;
+    } else {
+      clientX = event.clientX;
+      clientY = event.clientY;
+    }
+    mouseDown.current = event;
+    contextRef.current.beginPath();
+
+    const startX = clientX - rect.left;
+    const startY = clientY - rect.top;
+    contextRef.current.moveTo(startX, startY);
+  }
+
+  function handleMouseMove(event: React.MouseEvent | React.TouchEvent) {
+    if (!canvasRef.current || !contextRef.current || !mouseDown.current) {
+      return;
+    }
+    const canvas = canvasRef.current;
+    const rect = canvas.getBoundingClientRect();
+    contextRef.current.strokeStyle = "#ffffff";
+    let clientX = 0,
+      clientY = 0;
+    if ("touches" in event) {
+      clientX = event.touches[0].clientX;
+      clientY = event.touches[0].clientY;
+    } else {
+      clientX = event.clientX;
+      clientY = event.clientY;
+    }
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+    contextRef.current.lineTo(x, y);
+    contextRef.current.stroke();
+    // console.log(`Mouse position: ${x}, ${y}`);
+  }
+  function handleMouseUp() {
+    // if (!canvasRef.current || !mouseDown.current) {
+    //   return;
+    // }
+    mouseDown.current = null;
+    contextRef.current?.closePath();
+    // const canvas = canvasRef.current;
+    // const rect = canvas.getBoundingClientRect();
+    // const ctx = canvas.getContext("2d");
+    // if (!ctx) return;
+    // // ctx.reset();
+    // ctx.strokeStyle = "#ffffff";
+    // ctx.beginPath();
+    // const startX = mouseDown.current?.clientX - rect.left;
+    // const startY = mouseDown.current?.clientY - rect.top;
+    // ctx.moveTo(startX, startY);
+    // const x = event.clientX - rect.left;
+    // const y = event.clientY - rect.top;
+    // ctx.lineTo(x, y);
+    // ctx.stroke();
+    // ctx?.closePath();
+    // console.log(`Mouse position: ${x}, ${y}`);
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={gamutsLogo} className="logo" alt="Vite logo" />
-        </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>Gamuts</h1>
-      <p className="read-the-docs">
-        A modern, lightweight editor designed to make photo and video editing
-        faster, smoother, and more intuitive. Perfect for creators who value
-        speed without compromising quality.
-      </p>
-    </>
+    <div className="flex flex-col gap-4 items-center justify-center w-full h-full">
+      <a href="https://vite.dev" target="_blank">
+        <img src={gamutsLogo} className="logo" alt="Vite logo" />
+      </a>
+      <canvas
+        onTouchStart={handleMouseDown}
+        onTouchMove={handleMouseMove}
+        onTouchEnd={handleMouseUp}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        className="bg-black/20"
+        ref={canvasRef}
+      ></canvas>
+    </div>
   );
 }
 
