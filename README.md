c8ac4a1dc126fb473ca8d6e4db948ec3c53a07ac
Gokul Varadan <gokulvaradan2202@gmail.com>
2025-05-24 11:43:42 +0530

    initial planning

--- README.md
+++ README.md
@@ -1,22 +1,84 @@
 ## gamuts
 
+A simple tool that lets you sketch diagrams quickly with a hand-drawn feel and then transforms them into polished, professional-looking images perfect
+
 ### Requirements:
 
-- Hand Drawing Mode
-- Shapes & Put text inside shape
-- Infinite Zoom in-out
-- Image support
-  - Crop, Rounded, borders
-  - draw on top of image
-  - Invert
-  - Rotate
+- Free hand drawing tool
+  - Edit options
+    - Drawing style
+    - Line width
+    - color / gradients
+- Shapes - Circle, Rectangle, Rounded Rectangle, Pentagon, Hexagon, Lines, Arrows
+  - Operations:
+    - Reshape
+    - Rotate
+    - Color change
+    - Connect multiple shapes
+    - Create shapes while connecting
+    - Highlight selected shape
+    - Insert text inside the shape
+  - Edit options
+    - Circle
+      - Background color
+      - Stroke Color
+      - Stroke Style
+      - Stroke Width
+    - Rectangle / Pentagon / Hexagon
+      - Backgorund color
+      - Border radius
+      - Border style
+      - Border width
+    - Lines
+      - Line color
+    - Arrows
+      - Arrow head types
+      - Color
+- Frames
+
+  - Export options - pdf, svg, jpg
+  - Group creation
+
 - Text
+  - Font style
+  - Font color
+  - Background color
+  - Font size
+  - Resize
+  - Rotate
+- Image
+  - Drop images
+  - Resize
+  - Radius
+  - Filters
+    - Blur
+    - HUE
+    - Brighen
+    - Invert
+    - Saturation
+    - Noise
+    - Contrast
+    - Grayscale
+  - Crop
+- Infinite Canvas
+- Erase
+- Delete shapes / selections
+- Group selections
+- Copy
+- Cut
+- Paste
+- Undo/Redo
 
-### Decisions:
+- Video editing mode
+  - Create a scene
+  - Timeline
+  - Frame on each time
+  - Export - Mp4
 
-- React + Vite
-- Canvas / DOM:
-  - Canvas is faster
-  - Complex to implement everything from scratch
-  - DOM will be less efficient and cluttered
-  - Let's go with Canvas
+Architecture:
+
+Data Modal:
+
+Interfaces:
+
+Optimization:
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,64 @@
 import gamutsLogo from "/gamuts_logo.svg";
 import "./App.css";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
+// import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Konva from "konva";
+import {
+  Circle,
+  Layer,
+  Rect,
+  Text,
+  Stage,
+  Transformer,
+  Group,
+} from "react-konva";
 
-type Shape = {
-  id: number;
-  name: string;
-  coords: [number, number];
-  dimension: {
-    width: number;
-    height: number;
-  };
-  color: string;
-};
-
+// type Shape = {
+//   id: number;
+//   name: string;
+//   isSelected: boolean;
+//   coords: [number, number];
+//   dimension: {
+//     width: number;
+//     height: number;
+//   };
+//   color: string;
+// };
 function App() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const mouseDown = useRef<React.MouseEvent | React.TouchEvent>(null);
-  const contextRef = useRef<CanvasRenderingContext2D>(null);
-  const [shapes, setShapes] = useState<Array<Shape>>([]);
+  const canvasRef = useRef(null);
+  // const mouseDown = useRef<React.MouseEvent | React.TouchEvent>(null);
+  // const contextRef = useRef<CanvasRenderingContext2D>(null);
+  // const [shapes, setShapes] = useState<Array<Shape>>([]);
+  const rectRef = useRef<Konva.Rect>(null);
+  const trRef = useRef<Konva.Transformer>(null);
+
+  useEffect(() => {
+    if (!trRef.current || !rectRef.current) {
+      return;
+    }
+    const ref = trRef.current;
+    ref.nodes([rectRef.current]);
+  }, []);
+
   function setCanvasSize() {
     if (!canvasRef.current) {
       return;
     }
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - canvas.offsetTop;
+    console.log(canvasRef.current);
+    // const canvas = canvasRef.current;
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
   }
+
   useEffect(() => {
     if (!canvasRef.current) {
       return;
     }
 
-    const canvas = canvasRef.current;
+    // const canvas = canvasRef.current;
     setCanvasSize();
-    const ctx = canvas.getContext("2d");
-    contextRef.current = ctx;
+    // const ctx = canvas.getContext("2d");
+    // contextRef.current = ctx;
     // contextRef.current?.scale(2, 1);
     window.addEventListener("resize", setCanvasSize);
     return () => {
@@ -42,135 +66,155 @@ function App() {
     };
   }, [canvasRef]);
 
-  function handleMouseDown(event: React.MouseEvent | React.TouchEvent) {
-    if (!canvasRef.current || !contextRef.current) {
-      return;
-    }
-    const rect = canvasRef.current.getBoundingClientRect();
-    let clientX = 0,
-      clientY = 0;
-    if ("touches" in event) {
-      clientX = event.touches[0].clientX;
-      clientY = event.touches[0].clientY;
-    } else {
-      clientX = event.clientX;
-      clientY = event.clientY;
-    }
-    mouseDown.current = event;
-    contextRef.current.beginPath();
+  // function handleMouseDown(event: React.MouseEvent | React.TouchEvent) {
+  //   if (!canvasRef.current || !contextRef.current) {
+  //     return;
+  //   }
+  //   const rect = canvasRef.current.getBoundingClientRect();
+  //   let clientX = 0,
+  //     clientY = 0;
+  //   if ("touches" in event) {
+  //     clientX = event.touches[0].clientX;
+  //     clientY = event.touches[0].clientY;
+  //   } else {
+  //     clientX = event.clientX;
+  //     clientY = event.clientY;
+  //   }
+  //   mouseDown.current = event;
+  //   contextRef.current.beginPath();
 
-    const startX = clientX - rect.left;
-    const startY = clientY - rect.top;
-    contextRef.current.moveTo(startX, startY);
-  }
+  //   const startX = clientX - rect.left;
+  //   const startY = clientY - rect.top;
+  //   contextRef.current.moveTo(startX, startY);
+  // }
 
-  function handleMouseMove(event: React.MouseEvent | React.TouchEvent) {
-    if (!canvasRef.current || !contextRef.current || !mouseDown.current) {
-      return;
-    }
-    const canvas = canvasRef.current;
-    const rect = canvas.getBoundingClientRect();
-    contextRef.current.strokeStyle = "#ffffff";
-    let clientX = 0,
-      clientY = 0;
-    if ("touches" in event) {
-      clientX = event.touches[0].clientX;
-      clientY = event.touches[0].clientY;
-    } else {
-      clientX = event.clientX;
-      clientY = event.clientY;
-    }
-    const x = clientX - rect.left;
-    const y = clientY - rect.top;
-    contextRef.current.lineTo(x, y);
-    contextRef.current.stroke();
-    // console.log(`Mouse position: ${x}, ${y}`);
-  }
+  // function handleMouseMove(event: React.MouseEvent | React.TouchEvent) {
+  //   if (!canvasRef.current || !contextRef.current || !mouseDown.current) {
+  //     return;
+  //   }
+  //   const canvas = canvasRef.current;
+  //   const rect = canvas.getBoundingClientRect();
+  //   contextRef.current.strokeStyle = "#ffffff";
+  //   let clientX = 0,
+  //     clientY = 0;
+  //   if ("touches" in event) {
+  //     clientX = event.touches[0].clientX;
+  //     clientY = event.touches[0].clientY;
+  //   } else {
+  //     clientX = event.clientX;
+  //     clientY = event.clientY;
+  //   }
+  //   const x = clientX - rect.left;
+  //   const y = clientY - rect.top;
+  //   contextRef.current.lineTo(x, y);
+  //   contextRef.current.stroke();
+  //   // console.log(`Mouse position: ${x}, ${y}`);
+  // }
 
-  function handleMouseUp() {
-    // if (!canvasRef.current || !mouseDown.current) {
-    //   return;
-    // }
-    mouseDown.current = null;
-    contextRef.current?.closePath();
-    // const canvas = canvasRef.current;
-    // const rect = canvas.getBoundingClientRect();
-    // const ctx = canvas.getContext("2d");
-    // if (!ctx) return;
-    // // ctx.reset();
-    // ctx.strokeStyle = "#ffffff";
-    // ctx.beginPath();
-    // const startX = mouseDown.current?.clientX - rect.left;
-    // const startY = mouseDown.current?.clientY - rect.top;
-    // ctx.moveTo(startX, startY);
-    // const x = event.clientX - rect.left;
-    // const y = event.clientY - rect.top;
-    // ctx.lineTo(x, y);
-    // ctx.stroke();
-    // ctx?.closePath();
-    // console.log(`Mouse position: ${x}, ${y}`);
-  }
+  // function handleMouseUp() {
+  //   // if (!canvasRef.current || !mouseDown.current) {
+  //   //   return;
+  //   // }
+  //   mouseDown.current = null;
+  //   contextRef.current?.closePath();
+  //   // const canvas = canvasRef.current;
+  //   // const rect = canvas.getBoundingClientRect();
+  //   // const ctx = canvas.getContext("2d");
+  //   // if (!ctx) return;
+  //   // // ctx.reset();
+  //   // ctx.strokeStyle = "#ffffff";
+  //   // ctx.beginPath();
+  //   // const startX = mouseDown.current?.clientX - rect.left;
+  //   // const startY = mouseDown.current?.clientY - rect.top;
+  //   // ctx.moveTo(startX, startY);
+  //   // const x = event.clientX - rect.left;
+  //   // const y = event.clientY - rect.top;
+  //   // ctx.lineTo(x, y);
+  //   // ctx.stroke();
+  //   // ctx?.closePath();
+  //   // console.log(`Mouse position: ${x}, ${y}`);
+  // }
 
-  function draw() {
-    if (!contextRef.current) {
-      return;
-    }
-    for (const shape of shapes) {
-      const ctx = contextRef.current;
-      console.log(shape);
-      ctx.fillRect(
-        shape.coords[0],
-        shape.coords[1],
-        shape.dimension.width,
-        shape.dimension.height
-      );
-      ctx.fillStyle = "white";
-    }
-  }
+  // function draw() {
+  //   if (!contextRef.current) {
+  //     return;
+  //   }
+  //   for (const shape of shapes) {
+  //     const ctx = contextRef.current;
+  //     // console.log(shape);
+  //     ctx.fillRect(
+  //       shape.coords[0],
+  //       shape.coords[1],
+  //       shape.dimension.width,
+  //       shape.dimension.height
+  //     );
+  //     if (shape.isSelected) {
+  //       // Draw the border around the filled rectangle
+  //       ctx.strokeStyle = "blue";
+  //       ctx.lineWidth = 5;
+  //       // ctx.stroke();
+  //       ctx.strokeRect(
+  //         shape.coords[0] - 5,
+  //         shape.coords[1] - 5,
+  //         shape.dimension.width + 10,
+  //         shape.dimension.height + 10
+  //       );
+  //     }
+  //     ctx.fillStyle = "white";
+  //   }
+  // }
 
-  function handleClick(event: React.MouseEvent) {
-    if (!contextRef.current || !canvasRef.current) {
-      return;
-    }
-    const clientX = event.clientX,
-      clientY = event.clientY - canvasRef.current.offsetTop;
+  // function handleClick(event: React.MouseEvent) {
+  //   if (!contextRef.current || !canvasRef.current) {
+  //     return;
+  //   }
+  //   const clientX = event.clientX,
+  //     clientY = event.clientY - canvasRef.current.offsetTop;
 
-    const findObject = shapes.filter((shape: Shape, index) => {
-      const coords = shape.coords;
-      const dimension = shape.dimension;
-      // console.log(coords[0], dimension.width);
-      if (
-        coords[0] <= clientX &&
-        coords[0] + dimension.width >= clientX &&
-        coords[1] <= clientY &&
-        coords[1] + dimension.height >= clientY
-      ) {
-        shape.coords = [0, index * 2];
-        return true;
-      }
+  //   const findObject = shapes.filter((shape: Shape) => {
+  //     const coords = shape.coords;
+  //     const dimension = shape.dimension;
+  //     // console.log(coords[0], dimension.width);
+  //     if (
+  //       coords[0] <= clientX &&
+  //       coords[0] + dimension.width >= clientX &&
+  //       coords[1] <= clientY &&
+  //       coords[1] + dimension.height >= clientY
+  //     ) {
+  //       shape.dimension.width *= 2;
+  //       shape.dimension.height *= 2;
+  //       shape.isSelected = true;
+  //       // shape.coords = [0, index * 2];
+  //       return true;
+  //     }
 
-      return false;
-    });
+  //     return false;
+  //   });
 
-    if (findObject.length) {
-      contextRef.current.reset();
-      draw();
-      return;
-    }
-    console.log(findObject);
-    const newShape: Shape = {
-      id: shapes.length + 1,
-      name: "fillRect",
-      coords: [clientX, clientY],
-      dimension: {
-        width: 100,
-        height: 100,
-      },
-      color: "white",
-    };
-    setShapes([...shapes, newShape]);
-    draw();
-  }
+  //   if (findObject.length) {
+  //     contextRef.current.reset();
+  //     setShapes([...shapes]);
+  //     draw();
+  //     return;
+  //   }
+  //   console.log(findObject);
+  //   const newShape: Shape = {
+  //     id: shapes.length + 1,
+  //     name: "fillRect",
+  //     isSelected: false,
+  //     coords: [clientX, clientY],
+  //     dimension: {
+  //       width: 100,
+  //       height: 100,
+  //     },
+  //     color: "white",
+  //   };
+  //   setShapes([...shapes, newShape]);
+  // }
+
+  // useEffect(() => {
+  //   // draw();
+  // }, [shapes]);
 
   // function handleWheel(event: React.WheelEvent) {
   //   // event.preventDefault();
@@ -195,18 +239,74 @@ function App() {
       <a href="/">
         <img src={gamutsLogo} className="logo" alt="Vite logo" />
       </a>
-      <canvas
-        onClick={handleClick}
-        // onWheel={handleWheel}
-        onTouchStart={handleMouseDown}
-        onTouchMove={handleMouseMove}
-        onTouchEnd={handleMouseUp}
-        onMouseDown={handleMouseDown}
-        onMouseMove={handleMouseMove}
-        onMouseUp={handleMouseUp}
-        className="bg-black/20"
+      {/* <TransformWrapper>
+        <TransformComponent> */}
+      <Stage
         ref={canvasRef}
-      ></canvas>
+        width={window.innerWidth}
+        height={window.innerHeight}
+      >
+        <Layer>
+          <Group>
+            <Rect
+              ref={rectRef}
+              x={20}
+              y={50}
+              width={100}
+              onDblClick={(event) => console.log(event)}
+              height={100}
+              fill="white"
+              shadowBlur={10}
+              draggable={true}
+            ></Rect>
+            <Text
+              x={20}
+              y={50}
+              text="Try to drag shapes"
+              draggable
+              width={100}
+              fontSize={15}
+            />
+          </Group>
+          <Transformer
+            ref={trRef}
+            padding={6}
+            centeredScaling={true}
+            rotationSnapTolerance={100}
+            rotateAnchorOffset={20}
+            rotateLineVisible={false}
+            anchorSize={6}
+            borderStroke="green"
+            anchorCornerRadius={6}
+            anchorFill="green"
+            anchorStroke="green"
+            // anchorFill=""
+            // boundBoxFunc={(oldBox, newBox) => {
+            //   // limit resize
+            //   if (newBox.width > 200) {
+            //     return oldBox;
+            //   }
+            //   return newBox;
+            // }}
+          />
+          {/* <Text text="Try to drag shapes" draggable fontSize={15} />
+           */}
+          {/* <Arc
+            x={window.innerWidth / 2}
+            y={window.innerHeight / 2}
+            innerRadius={40}
+            outerRadius={70}
+            angle={60}
+            fill="yellow"
+            draggable={true}
+            stroke="black"
+            strokeWidth={4}
+          /> */}
+          <Circle x={200} y={100} radius={50} fill="white" draggable />
+        </Layer>
+      </Stage>
+      {/* </TransformComponent>
+      </TransformWrapper> */}
     </div>
   );
 }
